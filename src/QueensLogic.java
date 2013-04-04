c0c4f594017bccbfd6db91c518d15b9a43e3d4f5
Martin Faartoft <martin.faartoft@gmail.com>
2013-04-04 12:34:51 +0200

    adding code comments to queenslogic.java

--- src/QueensLogic.java
+++ src/QueensLogic.java
@@ -14,7 +14,9 @@ public class QueensLogic {
     private int[][] board;
     private final int nodes = 2000000;
     private final int cache = 200000; 
+    
     public BDDFactory f = JFactory.init(nodes, cache);
+    //initialized to TRUE, because the rules will be a big conjunction, and TRUE is is the identity for conjunctions 
     private BDD rules = f.one();
     private BDD restricted = f.one();
     private HashSet<Integer> queenPositions = new HashSet<Integer>();
@@ -34,50 +36,59 @@ public class QueensLogic {
         buildRules(x, y);
     }
 
+    //create the BDD representing the constraints of the N-Queens problem
+    //and store the result in the the "rules" class member
     private void buildRules(int x, int y) {
     	int numVariables = x * y; 
-    	f.setVarNum(numVariables);
+    	f.setVarNum(numVariables); //init BDD to have one variable per space on the chessboard
     	
-    	for(int n = 0; n < numVariables; n++) {
+    	for(int n = 0; n < numVariables; n++) { //foreach variable
+    		//conjoin existing rules with rule stating that variable n implies NOT (all the variables excluded by n)
     		rules.andWith(buildRule(n, getVarsExcludedBy(n)));
     	}
     	
+    	//conjoin existing rules with the rule that every row must have at least one queen
+    	//note that this is equivalent to "every columns must have at least one queen"
+    	//because of rotational symmetry
     	rules.andWith(oneQueenPerRowRule());
 	}
     
+    //get all the vars that is mutually exclusive with parameter 'var'
     private Iterable<Integer> getVarsExcludedBy(int var) {
-    	ArrayList<Integer> result = horizontal(var);
-    	result.addAll(vertical(var));
-    	result.addAll(diagonal(var));
+    	ArrayList<Integer> result = horizontal(var); //all variables on same row on the board
+    	result.addAll(vertical(var)); //all variables on same column
+    	result.addAll(diagonal(var)); //all variables lying on the 2 diagonals that intersect 'var'
     	
     	return result;
     }
 
 	public BDD buildRule(int i, Iterable<Integer> excludedVars) {
+		//initialized to TRUE, because the rule will be a big conjunction, and TRUE is is the identity for conjunctions
 		BDD rule = f.one();
 		
 		for (int excluded : excludedVars) {
-			rule.andWith(f.nithVar(excluded));
+			rule.andWith(f.nithVar(excluded)); //build the big conjunction of negated variables
 		}
 		
-		return f.ithVar(i).imp(rule); //x(i) implies !x(n) and !x(n+1)...
+		return f.ithVar(i).imp(rule); //add the "var 'i' implies" part
 	}
 	
 	public BDD oneQueenPerRowRule() {
 		BDD rule = f.one();
-		for (int row = 0; row < y; row++) {
-			BDD innerRule = f.zero();
+		for (int row = 0; row < y; row++) { //for each row
+			BDD innerRule = f.zero(); //init to FALSE, since this rule is a disjunction
 			
-			for (int col = 0; col < x; col++) {
-				innerRule.orWith(f.ithVar(row * x + col));
+			for (int col = 0; col < x; col++) { //for each column in row
+				innerRule.orWith(f.ithVar(row * x + col)); //x(i) OR x(i+1)...
 			}
 			
-			rule.andWith(innerRule);
+			rule.andWith(innerRule); //conjoin the individual row rules
 		}
 		
 		return rule;
 	}
 
+	//get all the variable indices on the same column as variable 'i'
 	public ArrayList<Integer> vertical(int i) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		int col = i % x;
@@ -91,6 +102,7 @@ public class QueensLogic {
 		return result;
 	}
 	
+	//get all the variable indices on the same row as variable 'i'
 	public ArrayList<Integer> horizontal(int i){
 		ArrayList<Integer> array = new ArrayList<Integer>();
 		for(int col = 0; col < x; col++) {
@@ -101,7 +113,7 @@ public class QueensLogic {
 		return array;
 	}
 	
-	
+	//get all the variable indices on the two diagonals intersecting variable 'i'
 	public ArrayList<Integer> diagonal(int i){
 		ArrayList<Integer> array = new ArrayList<Integer>();
 		
@@ -124,6 +136,7 @@ public class QueensLogic {
         return board;
     }
 	
+	//returns a BDD with all the variables where the user has placed queens, restricted to TRUE
 	private BDD getRestrictions() {
 		BDD res = f.one();
 		for (int q :  queenPositions) {
@@ -133,20 +146,24 @@ public class QueensLogic {
 		return res;
 	}
 	
+	//add a queen to the board at position 'var'
 	private void addQueen(int var) {
 		queenPositions.add(var);
 		updateRestrictions();
 	}
 	
+	//rebuild the restrictions, called when a queen is added or removed
 	private void updateRestrictions() {
 		restricted = rules.restrict(getRestrictions());
 	}
 	
+	//remove a queen from the board at position 'var'
 	private void removeQueen(int var) {
 		queenPositions.remove(var);
 		updateRestrictions();
 	}
 
+	//method called by QueensGUI, whenever a square is clicked
     public boolean insertQueen(int column, int row) {
     	
         if (board[column][row] == -1) { //clicked red cross, do nothing
@@ -161,15 +178,15 @@ public class QueensLogic {
             addQueen(row * x + column);
         }
         
-        boolean solved = restricted.pathCount() == 1;
+        boolean solved = restricted.pathCount() == 1; //if there's only one path leading to TRUE in the restricted BDD, consider the problem solved
         
-        for(int r = 0; r < y; r++) {
-	        for(int c = 0; c < x; c++) {
-	        	if(board[c][r] == 1) continue;
+        for(int r = 0; r < y; r++) { //foreach column
+	        for(int c = 0; c < x; c++) { //foreach row
+	        	if(board[c][r] == 1) continue; //if queen is present, skip this space
 	        	
-	        	if(restricted.restrict(f.ithVar(r * x + c)).isZero()) {
+	        	if(restricted.restrict(f.ithVar(r * x + c)).isZero()) { //if we cannot place a queen here, without making the problem unsolvable, add a red cross to the board
 	        		board[c][r] = -1;
-	        	} else if (solved) { 
+	        	} else if (solved) { //if only one solution remaining (and space is not a queen or a red cross), add a queen here
 	        		board[c][r] = 1;
 	        		addQueen(r * x + c);
 	        	} else {
